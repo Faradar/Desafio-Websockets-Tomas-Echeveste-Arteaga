831ca01e684924557084cb2aed7613d98221effb
Faradar <tomi_compuamste@hotmail.com>
2023-11-08 21:43:06 -0300

    fix: separated the websocket logic from app.js to websocket.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import handlebars from "express-handlebars";
-import { Server } from "socket.io";
 import { __dirname } from "./utils.js";
+import { configureWebSocket } from "./websocket.js";
 import productRouter from "./routes/product.router.js";
 import cartRouter from "./routes/cart.router.js";
 import viewsRouter from "./routes/views.router.js";
@@ -20,43 +20,10 @@ app.use("/api/products", productRouter);
 app.use("/api/carts", cartRouter);
 app.use("/", viewsRouter);
 
-import { ProductManager } from "./managers/product.manager.js";
-const productManager = new ProductManager(__dirname + "/data/products.json");
-
 const port = 8080;
 
 const httpServer = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-const server = new Server(httpServer);
-
-server.on("connection", async (socket) => {
-  console.log(`🟢 User connected ${socket.id}`);
-
-  try {
-    const products = await productManager.getProducts();
-    socket.emit("updateProducts", products);
-  } catch (error) {
-    console.error("Error fetching products:", error);
-  }
-
-  socket.on("addProduct", async (productData) => {
-    try {
-      const newProduct = await productManager.createProduct(productData);
-      if (newProduct) {
-        const updatedProducts = await productManager.getProducts(); // Updated to use 'await'
-        server.emit("updateProducts", updatedProducts);
-      } else {
-        socket.emit("productCreationFailed", "Failed to create the product.");
-      }
-    } catch (error) {
-      console.error("Error adding a new product:", error);
-      socket.emit("productCreationFailed", "Failed to create the product.");
-    }
-  });
-
-  socket.on("disconnect", () => {
-    console.log(`🔴 User disconnected ${socket.id}`);
-  });
-});
+configureWebSocket(httpServer);
---
+++ src/websocket.js
@@ -0,0 +1,39 @@
+import { Server } from "socket.io";
+import { __dirname } from "./utils.js";
+import { ProductManager } from "./managers/product.manager.js";
+
+const productManager = new ProductManager(__dirname + "/data/products.json");
+
+export function configureWebSocket(httpServer) {
+  const io = new Server(httpServer);
+
+  io.on("connection", async (socket) => {
+    console.log(`🟢 User connected ${socket.id}`);
+
+    try {
+      const products = await productManager.getProducts();
+      socket.emit("updateProducts", products);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+
+    socket.on("addProduct", async (productData) => {
+      try {
+        const newProduct = await productManager.createProduct(productData);
+        if (newProduct) {
+          const updatedProducts = await productManager.getProducts(); // Updated to use 'await'
+          io.emit("updateProducts", updatedProducts);
+        } else {
+          socket.emit("productCreationFailed", "Failed to create the product.");
+        }
+      } catch (error) {
+        console.error("Error adding a new product:", error);
+        socket.emit("productCreationFailed", "Failed to create the product.");
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`🔴 User disconnected ${socket.id}`);
+    });
+  });
+}
