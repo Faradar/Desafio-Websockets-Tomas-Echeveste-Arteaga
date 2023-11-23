616b524c37f6fe90152643c41a80df35c2dea366
Faradar <tomi_compuamste@hotmail.com>
2023-11-23 15:13:04 -0300

    feature: added a comment to connection.js in case a connection to compass is prefered over atlas

--- src/daos/mongodb/connection.js
+++ src/daos/mongodb/connection.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 const connectionString =
   "mongodb+srv://admin:admin@codercluster.q1e5hd6.mongodb.net/ecommerce?retryWrites=true&w=majority";
 
+// const connectionCompass = "mongodb://127.0.0.1:27017/ecommerce"; // Compass connection
+
 export const initMongoDB = async () => {
   try {
     await mongoose.connect(connectionString);
