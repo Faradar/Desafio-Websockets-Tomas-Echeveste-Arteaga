5af15b028f6fe19d54d1de7967e5ed68cc096298
Faradar <tomi_compuamste@hotmail.com>
2023-12-07 18:19:26 -0300

    feature: added bcrypt for password security

--- src/daos/mongodb/session.dao.js
+++ src/daos/mongodb/session.dao.js
@@ -1,11 +1,16 @@
 import { UserModel } from "./models/user.model.js";
+import { createHash, isValidPass } from "../../utils.js";
 
 export default class SessionDaoMongoDB {
   async register(user) {
     try {
       const { email } = user;
       const exists = await UserModel.findOne({ email });
-      if (!exists) return await UserModel.create(user);
+      if (!exists)
+        return await UserModel.create({
+          ...user,
+          password: createHash(user.password),
+        });
       else return false;
     } catch (error) {
       console.error(`Error registering ${user} user: ${error.message}`);
@@ -15,12 +20,11 @@ export default class SessionDaoMongoDB {
 
   async login(email, password) {
     try {
-      // if (email === "adminCoder@coder.com" && password === "adminCod3r123") {
-      //   return await UserModel.create({ ...user, role: "admin" });
-      // }
-      const userExist = await UserModel.findOne({ email, password });
-      if (!userExist) return false;
-      else return userExist;
+      const userExist = await UserModel.findOne({ email });
+      if (userExist) {
+        const pass = isValidPass(password, userExist);
+        return !pass ? false : userExist;
+      } else return false;
     } catch (error) {
       console.error(
         `Error logging in the ${email} and ${password} credentials: ${error.message}`
